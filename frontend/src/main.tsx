2a01d107e1b4c3b67d4117297047e7e4928fef40
Isaac-Chew <Isaacchew@utexas.edu>
2025-12-04 15:16:11 -0600

    addjusted main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,15 @@ import "./index.css";
 import { AsgardeoProvider } from "@asgardeo/react";
 import { BrowserRouter } from "react-router-dom";
 
+const origin = window.location.origin; // works for localhost and Render
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AsgardeoProvider
       clientId={import.meta.env.VITE_ASGARDEO_CLIENT_ID}
       baseUrl={import.meta.env.VITE_ASGARDEO_BASE_URL}
+      afterSignInUrl={`${origin}/rider`}
+      afterSignOutUrl={`${origin}/`}
     >
       <BrowserRouter>
         <App />
